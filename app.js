b162a380f039baff71935070abf162f2577d31bc
Dealbreak <121974359+Dealbreak@users.noreply.github.com>
2026-09-22 08:11:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -260,6 +260,13 @@ async function parseStepAndCalcMetrics(uint8Array) {
             totalArea += Math.sqrt(cross[0]*cross[0] + cross[1]*cross[1] + cross[2]*cross[2]) / 2.0;
         }
     }
+    printersCountInput.onchange = (e) => {
+    let val = parseInt(e.target.value) || 1;
+    if (val > 10) val = 10;
+    if (val < 1) val = 1;
+    e.target.value = val;
+    calculateAndDisplay();
+};
     
     const bbVolume = (maxX - minX) * (maxY - minY) * (maxZ - minZ);
     
